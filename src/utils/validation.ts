9d5706fb4ab8f0e245c47ca38469b49ae8ba91fd
Tran Quang <quang.tran@realstake.io>
2022-09-10 10:25:14 +0700

    Add handle validation error

--- src/utils/validation.ts
+++ src/utils/validation.ts
@@ -1,5 +1,18 @@
 import { ZodError, ZodType } from "zod";
+import { HTTPError } from "../errors/base";
+import { Error } from "mongoose";
+import { StatusCodes } from "http-status-codes";
 
 export function validateZod<T>(schema: ZodType<T>, input: unknown) {
-    return schema.parse(input);
+    try {
+        return schema.parse(input);
+    } catch (error) {
+        if (error instanceof ZodError) {
+            const firstIssue = error.issues[0];
+            const fieldPath = firstIssue.path.join(".");
+            const errorMessage = `[${fieldPath}] ${firstIssue.message}`;
+            throw new HTTPError([StatusCodes.BAD_REQUEST, errorMessage]);
+        }
+        throw new Error(error as string);
+    }
 }
