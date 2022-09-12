888d2122407bf619731b8032a00db4813aa7853f
Tran Quang <quang.tran@realstake.io>
2022-09-12 11:54:29 +0700

    Fix cors

--- src/app.ts
+++ src/app.ts
@@ -15,7 +15,7 @@ export async function initApp() {
     const store = new MongoDBStore({ uri: getUri(), collection: "sessions" });
     store.on("error", (error) => console.log(error));
 
-    app.use(cors());
+    app.use(cors({ credentials: true, origin: "*" }));
     app.use(morgan("combined"));
     app.use(express.json());
     app.use(
